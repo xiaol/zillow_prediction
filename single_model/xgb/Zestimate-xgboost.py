357a41d815f1fc88c3a4c9923818e720163c2f6b
xiaol <liuxiao.in@qq.com>
2017-08-18 17:22:41 +0800

    tune logerror

--- single_model/xgb/Zestimate-xgboost.py
+++ single_model/xgb/Zestimate-xgboost.py
@@ -82,7 +82,8 @@ del x_train, x_valid; gc.collect()
 
 print('Training ...')
 
-params = {'eta': 0.02, 'objective': 'reg:linear', 'eval_metric': 'mae', 'min_child_weight': 1, 'colsample_bytree': 0.2, 'max_depth': 7, 'lambda': 0.3, 'alpha': 0.6, 'silent': 1}
+params = {'eta': 0.02, 'objective': 'reg:linear', 'eval_metric': 'mae', 'min_child_weight': 1,  'max_depth': 7, 'subsample':0.8, 'lambda':0.8, 'alpha':0.4, 'silent': 1}
+print(params)
 
 watchlist = [(d_train, 'train'), (d_valid, 'valid')]
 clf = xgb.train(params, d_train, 10000, watchlist, early_stopping_rounds=100, verbose_eval=10)
