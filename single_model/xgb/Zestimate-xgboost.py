5e98544ad6571d3091c88f93471e2b4072eab13f
xiaol <liuxiao.in@qq.com>
2017-08-22 14:49:56 +0800

    N 300

--- single_model/xgb/Zestimate-xgboost.py
+++ single_model/xgb/Zestimate-xgboost.py
@@ -50,8 +50,8 @@ def get_features(df):
     for col in ['finishedsquarefeet6', 'finishedsquarefeet12', 'finishedsquarefeet13', 'finishedsquarefeet15',
                 'finishedsquarefeet50', 'garagetotalsqft', 'lotsizesquarefeet', 'yardbuildingsqft17', 'yardbuildingsqft26',
                 'taxamount', 'taxvaluedollarcnt', 'landtaxvaluedollarcnt', 'structuretaxvaluedollarcnt', 'yearbuilt',
-                'basementsqft', 'finishedfloor1squarefeet', 'calculatedfinishedsquarefeet', 'taxamount',
-                'structuretaxvaluedollarcnt', 'landtaxvaluedollarcnt']:
+                'basementsqft', 'finishedfloor1squarefeet', 'calculatedfinishedsquarefeet', 'tax_rt', 'structure_tax_rt',
+                'land_tax_rt','taxamount','structuretaxvaluedollarcnt', 'landtaxvaluedollarcnt']:
         df = merge_mean(df, ['loc_label'], col, 'loc_'+col+'_mean')
 
     return df
