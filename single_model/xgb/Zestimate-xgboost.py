7d24d18c35e1f2c30707548558e8de849d135fce
xiaol <liuxiao.in@qq.com>
2017-08-24 11:33:08 +0800

    test region property num

--- single_model/xgb/Zestimate-xgboost.py
+++ single_model/xgb/Zestimate-xgboost.py
@@ -40,8 +40,8 @@ def get_features(df):
 
     # 商圈内待售房屋数量
     df = merge_nunique(df, ['loc_label'], 'parcelid', 'loc_building_num')
-    '''
     df = merge_nunique(df, ['regionidzip'], 'parcelid', 'region_property_num')
+    '''
     df = merge_nunique(df, ['regionidcity'], 'parcelid', 'city_property_num')
     df = merge_nunique(df, ['regionidcounty'], 'parcelid', 'county_property_num')
     '''
