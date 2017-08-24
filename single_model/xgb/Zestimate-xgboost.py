ef1f75c5f719dff0b3833fdcbda37063441b35a7
xiaol <liuxiao.in@qq.com>
2017-08-24 12:49:23 +0800

    test city_month_transaction count

--- single_model/xgb/Zestimate-xgboost.py
+++ single_model/xgb/Zestimate-xgboost.py
@@ -44,7 +44,7 @@ def get_features(df):
     df = merge_nunique(df, ['regionidcity'], 'parcelid', 'city_property_num')
     # df = merge_nunique(df, ['regionidcounty'], 'parcelid', 'county_property_num')
 
-    df = merge_nunique(df, ['regionidcity'], 'transaction_month', 'city_month_transaction_count')
+    df = merge_nunique(df, ['transaction_month'], 'regionidcity', 'city_month_transaction_count')
     # 商圈房屋状况均值
     # df = merge_median(df, ['regionidcity'], 'buildingqualitytypeid', 'city_quality_median')
     '''
